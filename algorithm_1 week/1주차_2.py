4ab249f14eb556bd95c517998bda34de89cb1b37
whtmdgn1409 <whtmdgn1409@gmail.com>
2022-04-03 16:03:22 +0900

    알고리즘 스터디 1주차 level 2~

---
+++ algorithm_1 week/1주차_2.py
@@ -0,0 +1,28 @@
+Grade = str(input())
+
+if Grade == 'A+':
+    print(4.3)
+elif Grade == 'A0':
+    print(4.0)
+elif Grade == 'A-':
+    print(3.7)
+elif Grade == 'B+':
+    print(3.3)
+elif Grade == 'B0':
+    print(3.0)
+elif Grade == 'B-':
+    print(2.7)
+elif Grade == 'C+':
+    print(2.3)
+elif Grade == 'C0':
+    print(2.0)
+elif Grade == 'C-':
+    print(1.7)
+elif Grade == 'D+':
+    print(1.3)
+elif Grade == 'D0':
+    print(1.0)
+elif Grade == 'D-':
+    print(0.7)
+elif Grade == 'F':
+    print(0.0)
